71e4c7fbc11c14d0dcf7a90cc3aa11d4be8097c7
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-28 15:43:18 +0100

    printing on page projects

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <script>
-  // import something from './components/';
+  import AppHeader from './components/AppHeader.vue';
   import axios from 'axios';
 const baseEndpoint = 'http://localhost:8000/api/projects/'
   export default {
     name: 'Boolfolio',
     data() {
       return {
-
+        projects: []
       }
     },
-    components: {},
+    components: {AppHeader},
     methods: {
       fetchProjects() {
         axios.get(baseEndpoint).then((res) => {
           console.log(res.data)
+          this.projects = res.data
         })
       }
     },
@@ -24,7 +25,16 @@ const baseEndpoint = 'http://localhost:8000/api/projects/'
 </script>
 
 <template>
-  <button class="btn btn-primary">Funziona</button>
+  <!-- header -->
+  <AppHeader/>
+  <!-- header -->
+
+  <div class="container my-4">
+    <p v-for="project in projects">
+      {{ project.title }}
+    </p>
+  </div>
+
 </template>
 
 <style lang='scss' scoped>
